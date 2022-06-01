4282a24207c92f17355719cfc7bda311e9aa0b31
qcgithubtest2018 <106365872+qcgithubtest2018@users.noreply.github.com>
2022-06-01 11:05:52 +0530

    Final Commit

--- src/pkg1/Manager1.java
+++ src/pkg1/Manager1.java
@@ -6,6 +6,8 @@ public class Manager1
 	{
 		System.out.println("1");
 		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 		System.out.println("Done by Developer");
 	}
 }
